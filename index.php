cd6a07f6c3663d86dba41da1dd5606cce4ccae4c
Fitria Nurwidyawati <fitrianurw@users.noreply.github.com>
2014-05-04 10:40:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -51,7 +51,6 @@
 <?php
 // we can specify a localy version or a remote xml as in example
 $xml = simplexml_load_file("http://www.antara.co.id/rss/news.xml");
-// if I use a local file saved in /tmp directory: $xml = simplexml_load_file("/tmp/podcast.xml");
  
 // now make a XPath selection: select all <item> tag.
 $items = $xml->xpath("/rss/channel/item");
